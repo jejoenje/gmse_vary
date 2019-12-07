c923da1603494819ac659b55d62d83eb9c5af52b
jejoenje <jeroenminderman@gmail.com>
2019-12-07 14:39:08 +0000

    Discovered major bug in set_land() function, with one users' land not being filled in equally. Consequences seem relatively minor but will have affected all land-varying simulations so far. Will need to re-do all of these!

--- gmse_apply_helpers.R
+++ gmse_apply_helpers.R
@@ -37,7 +37,7 @@ init_sims = function(SIM_NAME) {
   dir.create(outpath)
   ### Save current parameters to output dir (note Rdata file to separate from Rds sim files)
   save(gmse_paras, file=sprintf("%sparas_%s_%s.Rdata", outpath, outidx, SIM_NAME))
- 
+  
   assign("outdir", outdir, env = globalenv())
   assign("outidx", outidx, env = globalenv())
   assign("outpath", outpath, env = globalenv())
@@ -157,46 +157,47 @@ yield_to_return = function(yield, yield_return, type = "direct") {
 set_land = function(land, s, type, hi_frac = NULL, up_frac = NULL, lo_frac = NULL) {
   pub_land_present = FALSE
   tland = table(land)
-
+  
   # If assuming equal land dist, return what was given:
   if(type == "equal" | is.null(type) ) {
     return(land)
   }
-
+  
   if(type == "oneRich") {
-
+    
     if("1" %in% names(tland)) {
       pub_land_present = TRUE
       common_land = tland[1]
       tland = tland[2:len(tland)]
-      names(tland) = as.character(1:len(tland))
+      #names(tland) = as.character(1:len(tland))
     } 
     
     pland = sum(tland)
     if(is.null(up_frac)) up_frac = hi_frac+(hi_frac*0.07)
     if(is.null(lo_frac)) lo_frac = hi_frac-(hi_frac*0.07)
-
+    
     rich_frac = rgbeta(1, mean = hi_frac, var = hi_frac/2000, min = lo_frac, max = up_frac)
     rich_size = floor(pland*rich_frac)
-
+    
     rest = pland - rich_size
-
-    others = rep(NA, s-1)
-    others[1] = ceiling(rest/(s-1))
-    rest = rest-others[1]
-    for(i in 2:len(others)) {
-      others[i] = ceiling(rest/(s-i))
-      rest = rest-others[i]
-    }
-    all = c(others, rich_size)
-
+    
+    others = rest/(s-1)
+    others = floor(others)
+    
+    all = c(rich_size, rep(others, s-1))
+    all[1] = all[1]+(pland-sum(all))
+    
     if(pub_land_present == TRUE) {
       all = c(common_land, all)
-    }
-    
-    all_cells = as.vector(NULL)
-    for(i in 1:len(all)) {
-      all_cells = c(all_cells, rep(i, all[i]))
+      all_cells = as.vector(NULL)
+      for(i in 1:len(all)) {
+        all_cells = c(all_cells, rep(i, all[i]))
+      }
+    } else {
+      all_cells = as.vector(NULL)
+      for(i in 1:len(all)) {
+        all_cells = c(all_cells, rep(i+1, all[i]))
+      }  
     }
     
     land_out = matrix(all_cells, nrow = nrow(land), ncol = ncol(land))
