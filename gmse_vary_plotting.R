ea0a18399ed0a5233a56816f02b52f35b7ad1bb8
jejoenje <jeroenminderman@gmail.com>
2020-03-16 13:03:48 +0000

    Setting up for new refined simulation set; added document table and started scenario1_new.R. Need to look at how set_budget() would work with new set_man_budget() ('tax') version.

--- gmse_vary_plotting.R
+++ gmse_vary_plotting.R
@@ -17,3 +17,45 @@ gmse_vary_plot(sims0, type = "pop", col = "darkred", ylab = "Population", xlab =
 gmse_vary_plot(sims0, type = "yield", col = "darkgreen", sumtype = "median", ylab = "Yield", xlab = "Time step")
 gmse_vary_plot(sims0, type = "budget", col = "darkblue", sumtype = "median", ylab = "Budget", xlab = "Time step")
 
+
+# Scenario 1:
+folder = "sims/scenario1"
+# Read output files in folder
+sims1 = gmse_rds_summary(folder)
+# Number of simulations
+length(sims1)
+# Number of (realised) years per sim:
+unlist(lapply(sims1, function(x) length(x$pop[,1]) ))
+
+gmse_vary_plot(sims1, type = "pop", col = "darkred", ylab = "Population", xlab = "Time step")
+gmse_vary_plot(sims1, type = "yield", col = "darkgreen", sumtype = "median", ylab = "Yield", xlab = "Time step")
+gmse_vary_plot(sims1, type = "budget", col = "darkblue", sumtype = "median", ylab = "Budget", xlab = "Time step")
+
+
+# Scenario 2a:
+folder = "sims/scenario2a"
+# Read output files in folder
+sims2a = gmse_rds_summary(folder)
+# Number of simulations
+length(sims2a)
+# Number of (realised) years per sim:
+unlist(lapply(sims2a, function(x) length(x$pop[,1]) ))
+
+gmse_vary_plot(sims2a, type = "pop", col = "darkred", ylab = "Population", xlab = "Time step")
+gmse_vary_plot(sims2a, type = "yield", col = "darkgreen", sumtype = "median", ylab = "Yield", xlab = "Time step")
+gmse_vary_plot(sims2a, type = "budget", col = "darkblue", sumtype = "median", ylab = "Budget", xlab = "Time step")
+
+
+# Scenario 2a:
+folder = "sims/scenario2b"
+# Read output files in folder
+sims2b = gmse_rds_summary(folder)
+# Number of simulations
+length(sims2b)
+# Number of (realised) years per sim:
+unlist(lapply(sims2b, function(x) length(x$pop[,1]) ))
+
+gmse_vary_plot(sims2b, type = "pop", col = "darkred", ylab = "Population", xlab = "Time step")
+gmse_vary_plot(sims2b, type = "yield", col = "darkgreen", sumtype = "median", ylab = "Yield", xlab = "Time step")
+gmse_vary_plot(sims2b, type = "budget", col = "darkblue", sumtype = "median", ylab = "Budget", xlab = "Time step")
+
--- scenario1_new.R
+++ scenario1_new.R
@@ -1,21 +1,20 @@
 rm(list=ls())
 
 ### Set and create output folder for scenario run:
-scenario_name = "scenario_TEST"
+scenario_name = "scenario1_new"
 out_path = paste0("./sims/",scenario_name)
 
 library(GMSE)
 library(scales)
 library(RColorBrewer)
 library(doParallel)
-library(truncnorm)
 source('helpers.R')
 source('gmse_apply_helpers.R')
 source("build_para_grid.R")
 
 ### Override basic parameters according to scenario:
-gmse_paras$res_move_type = 0
-gmse_paras$res_move_to_yield = TRUE
+# gmse_paras$res_move_type = 0
+# gmse_paras$res_move_to_yield = TRUE
 
 # Create root output dir for scenario_name, if not create it:
 if(!dir.exists(out_path)) {
@@ -27,39 +26,27 @@ n_years = gmse_paras$n_years
 # Initialise simulation run with first time step (i.e. time = 0)
 sim_old = init_sims(gmse_paras)
 
-# Reset user budgets (truncated normal):
-start_budgets = rtruncnorm(gmse_paras$stakeholders, 
-                           a = 0.8*gmse_paras$user_budget, 
-                           b = 1.2*gmse_paras$user_budget, 
-                           mean = gmse_paras$user_budget, 
-                           sd = gmse_paras$user_budget/10)
-sim_old$AGENTS[2:nrow(sim_old$AGENTS),17] = start_budgets
-
-# Set manager budget according to the user budgets above:
-sim_old$AGENTS[1,17] = set_man_budget(start_budgets, type = "prop", p = 0.125)
-
 # Initialise output for simulation run:
 yr_res = init_sim_out(sim_old)
 
-###### REDISTRIBUTE LAND HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-###### REDISTRIBUTE LAND HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-###### REDISTRIBUTE LAND HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-###### REDISTRIBUTE LAND HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 # Loop through nunmber of years
 for(i in 1:n_years) {
-  
+
+  #print(sprintf("Time step %d", i))
+    
   ### Move resources according to yield
-  sim_old = move_res(sim_old, gmse_paras)
+  #sim_old = move_res(sim_old, gmse_paras)
   
   ### Set next time step's user budgets
-  new_b = set_budgets(cur = sim_old, type = "2020", yield_type = "linear", yv = 1)
-  sim_old$AGENTS[2:nrow(sim_old$AGENTS),17] = new_b
+  # new_b = set_budgets(cur = sim_old, type = "2020", yield_type = "linear", yv = 1)
+  # new_b[new_b>10000] = 10000
+  # new_b[new_b<gmse_paras$minimum_cost] = gmse_paras$minimum_cost
+  # 
+  # sim_old$AGENTS[2:nrow(sim_old$AGENTS),17] = new_b
   
   ### Set next time step's manager's budget, according to new user budgets
-  #sim_old$AGENTS[1,17] = set_man_budget(u_buds = new_b, type = "mean")
-  sim_old$AGENTS[1,17] = set_man_budget(u_buds = new_b, type = "prop", p = 0.15)
-  
+  # sim_old$AGENTS[1,17] = set_man_budget(u_buds = new_b, type = "mean")
+
   ### Try to run next time step
   sim_new = try({gmse_apply(get_res = "Full", old_list = sim_old)}, silent = T)
 
@@ -91,6 +78,6 @@ tstamp = sub("\\.","",tstamp)
 saveRDS(yr_res, file = paste0(out_path,"/",tstamp,".Rds"))
 
 # To run 30 of this script in parallel:
-#  seq 100 | xargs -I{} -P 6 /usr/bin/Rscript scenario2.R
+#  seq 100 | xargs -I{} -P 6 /usr/bin/Rscript scenario2a.R
 
 
